679c52aec26b91508ad787b876669a12a0ce2e4e
perry <pkc5102@psu.edu>
2021-05-10 00:00:50 -0400

    Added some comments for readability

--- src/bot.js
+++ src/bot.js
@@ -1,11 +1,26 @@
 require('dotenv').config();
 
+// Create an instance of the bot to interact with the server
 const { Client } = require('discord.js');
 const client = new Client();
 
+// Register bot using bot token
+client.login(process.env.BOT_TOKEN)
+
+// Sends a message when the client is connected
 client.on('ready', () => {
     console.log(`The bot has been connected with username: ${client.user.username}`);
 });
+
+/* Message function
+params: 
+message: message any user sends in any channel
+data: any data required for the message
+channel: set a specific channel to listen to
+
+Can set up commands like this, may be a better way but
+this works for now.
+*/
 
 client.on('message', (message) => {
     console.log(`[${message.author.tag}] : ${message.content}`)
@@ -13,5 +28,3 @@ client.on('message', (message) => {
         message.channel.send('Test response');
     }
 });
-
-client.login(process.env.BOT_TOKEN)
