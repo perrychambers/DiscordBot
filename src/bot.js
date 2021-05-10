7790014cac4701e6d582a9c78034d0d20744b762
perry <pkc5102@psu.edu>
2021-05-09 23:52:37 -0400

    Testing message responses

--- src/bot.js
+++ src/bot.js
@@ -3,4 +3,15 @@ require('dotenv').config();
 const { Client } = require('discord.js');
 const client = new Client();
 
+client.on('ready', () => {
+    console.log(`The bot has been connected with username: ${client.user.username}`);
+});
+
+client.on('message', (message) => {
+    console.log(`[${message.author.tag}] : ${message.content}`)
+    if(message.content === 'test') {
+        message.channel.send('Test response');
+    }
+});
+
 client.login(process.env.BOT_TOKEN)
